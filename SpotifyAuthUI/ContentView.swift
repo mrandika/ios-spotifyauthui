e445996eb1b1fb8f848fcacb85ec0c26133c00a5
Muhammad Rizki Andika <mrizkiandika226@gmail.com>
2020-12-30 15:45:04 +0700

    Initial Commit

---
+++ SpotifyAuthUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpotifyAuthUI
+//
+//  Created by Andika on 30/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
